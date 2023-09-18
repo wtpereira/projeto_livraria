a31bc28e26b31fd5b6b47e2a56eb0124a55d20e4
Wellington <wtpereira@gmail.com>
2023-09-18 20:29:23 -0300

    Corrigir o bug no 'buscar_por_id' do 'livro_dao'.

--- dao/livro_dao.py
+++ dao/livro_dao.py
@@ -58,7 +58,10 @@ class LivroDAO:
         cursor.execute("SELECT titulo, isbn, paginas, ano, resumo, categoria_id, editora_id, autor_id, id FROM livros WHERE id = %s", (livro_id,))
         resultado = cursor.fetchone()
         if (resultado):
-            livro = Livro(resultado[0], resultado[1], resultado[2], resultado[3], resultado[4], resultado[5], resultado[6], resultado[7], resultado[8])
+            categoria = CategoriaDAO().buscar_por_id(resultado[5])
+            editora = EditoraDAO().buscar_por_id(resultado[6])
+            autor = AutorDAO().buscar_por_id(resultado[7])
+            livro = Livro(resultado[0], resultado[1], resultado[2], resultado[3], resultado[4], categoria, editora, autor, resultado[8])
 
         cursor.close()
         conexao.close()
